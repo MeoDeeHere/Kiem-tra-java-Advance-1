89ca04de5ff45f77566d17b8cf1cb5c08810c9b3
MeoDeeHere <126169885+MeoDeeHere@users.noreply.github.com>
2023-03-01 16:37:14 +0700

    Add files via upload

---
+++ test3-1/src/main/java/bai1/Main.java
@@ -0,0 +1,10 @@
+package bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        circle ht = new circle();
+        //ht.setRadius(-2);
+        ht.setRadius(4);
+        System.out.println(ht);
+    }
+}
